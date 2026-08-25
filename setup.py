69b8975b5b36f90f8123e824a2eba15d97cef98d
Andras Karolyi <andraskarolyi@hotmail.com>
2026-08-25 05:11:36 +0200

    Bump plugin version to 0.7.8

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoklipscreen_bridge"
 plugin_name = "Octoklipscreen Bridge"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.7.7"
+plugin_version = "0.7.8"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
