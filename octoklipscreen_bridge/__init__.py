773ea80152aa27ef24f8c4d1d124c8bba98528ac
Andras Karolyi <andraskarolyi@hotmail.com>
2026-08-26 13:41:07 +0200

    Bump version to 0.8.4 in update information

--- octoklipscreen_bridge/__init__.py
+++ octoklipscreen_bridge/__init__.py
@@ -675,11 +675,11 @@ class OctoklipscreenBridgePlugin(
     return dict(
         octoklipscreen_bridge=dict(
             displayName="Octoklipscreen Bridge",
-            displayVersion="0.8.3",
+            displayVersion="0.8.4",
             type="github_release",
             user="karolyia79",
             repo="OctoklipscreenBridge",
-            current="0.8.3",
+            current="0.8.4",
             stable_branch=dict(
                 name="Main", branch="main", commitish=["main"]
             ),  # Javítva commitish-re
